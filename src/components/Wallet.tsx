38bb40a2b4ea2716ca2eae3caf81ab6caa9a09c4
Ashish Maurya <143325254+M-Captain@users.noreply.github.com>
2025-05-24 13:09:03 +0000

    Fix Netlify build config

--- src/components/Wallet.tsx
+++
@@ -1,9 +0,0 @@
-import { ConnectButton, useWalletKit } from "@mysten/wallet-kit";
-
-export default function AccountDisplay() {
-    return (
-        <ConnectButton
-        connectText = {'Connect Wallet'}
-        />
-    );
-}
