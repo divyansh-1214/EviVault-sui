bb797c05299015cf0c61a06307f1098389febe7a
Ashish Maurya <143325254+M-Captain@users.noreply.github.com>
2025-05-24 12:47:51 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -37,9 +37,9 @@ function App() {
     <div>
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route
-          path="/evidence"
+          path="/"
           element={
             <>
               <h1>Evidence Management System</h1>
